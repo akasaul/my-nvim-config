6edd539b6046d5fcf05304ff8255b413f2874f8b
Nikodimos Jemaneh <nikodimosjemaneh40@gmail.com>
2024-07-28 12:35:54 +0300

    harpoon and telescope setup

--- lua/niko/plugins/telescope.lua
+++ lua/niko/plugins/telescope.lua
@@ -7,6 +7,7 @@ return {
     "nvim-tree/nvim-web-devicons",
     "folke/todo-comments.nvim",
     "telescope-dap.nvim",
+    "smartpde/telescope-recent-files",
   },
   config = function()
     local telescope = require("telescope")
@@ -45,6 +46,7 @@ return {
 
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+    keymap.set("n", "<leader>b", "<cmd>Telescope buffers<cr>", { desc = "Fuzzy find buffers" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
