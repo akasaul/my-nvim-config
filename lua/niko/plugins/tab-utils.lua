dd7ef165a9f2cf7d96e892a309acfde6dce078e7
Nikodimos Jemaneh <nikodimosjemaneh40@gmail.com>
2024-07-28 15:18:12 +0300

    tab utils added

---
+++ lua/niko/plugins/tab-utils.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "ThePrimeagen/harpoon",
+    config = function()
+      local mark = require("harpoon.mark")
+      local ui = require("harpoon.ui")
+      local keymap = vim.keymap -- for conciseness
+
+      keymap.set("n", "<leader>a", mark.add_file)
+      keymap.set("n", "<C-m>", ui.toggle_quick_menu)
+
+      keymap.set("n", "<leader>h1", function()
+        ui.nav_file(1)
+      end)
+      keymap.set("n", "<leader>h2", function()
+        ui.nav_file(2)
+      end)
+      keymap.set("n", "<leader>h3", function()
+        ui.nav_file(3)
+      end)
+      keymap.set("n", "<leader>h4", function()
+        ui.nav_file(4)
+      end)
+    end,
+  },
+  {
+    "axkirillov/hbac.nvim",
+    config = true,
+  },
+}
